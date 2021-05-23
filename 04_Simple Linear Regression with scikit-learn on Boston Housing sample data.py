3806c63a2ce86e74dc4c562f38cc299a0a5172c4
Milad Korde <38597478+Milad84@users.noreply.github.com>
2021-05-23 13:35:40 -0500

    Create 04_Simple Linear Regression with scikit-learn on Boston Housing sample data.py

---
+++ 04_Simple Linear Regression with scikit-learn on Boston Housing sample data.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      milad
+#
+# Created:     01/01/2021
+# Copyright:   (c) milad 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
+boston = datasets.load_boston()
+X_train = boston['data']
+y_train = boston['target']
+
+from sklearn.linear_model import LinearRegression
+sklearn_model = LinearRegression()
+sklearn_model.fit(X_train, y_train);
+
+sklearn_predictions = sklearn_model.predict(X_train)
+fig, ax = plt.subplots()
+sns.scatterplot(y_train, sklearn_predictions)
+ax.set_xlabel(r'$y$', size = 16)
+ax.set_ylabel(r'$\hat{y}$', rotation = 0, size = 16, labelpad = 15)
+ax.set_title(r'$y$ vs. $\hat{y}$', size = 20, pad = 10)
+sns.despine()
+
+predictors = boston.feature_names
+beta_hats = sklearn_model.coef_
+print('\n'.join([f'{predictors[i]}: {round(beta_hats[i], 3)}' for i in range(3)]))
+
